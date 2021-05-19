c9c63ffe3554709e06b697cdcbfe00ca166bad78
Bill Adams <vikingbill@gmail.com>
2021-05-19 19:39:53 -0400

    finish phase 1 of routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,16 @@
 // backend/routes/index.js
 const express = require('express');
+
+const apiRouter = require('./api');
+
 const router = express.Router();
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
+router.use('/api', apiRouter);
+
 
 module.exports = router;
