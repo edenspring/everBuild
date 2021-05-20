e3e80d6ca9894a4a37e930ef4c7ec991609dd3c5
Bill Adams <vikingbill@gmail.com>
2021-05-20 17:48:08 -0400

    add restore user on refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
+import * as sessionActions from "./store/session"
 import LoginFormPage from "./components/LoginFormPage";
 
 function App() {
+
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(()=>{
+    dispatch(sessionActions.restoreUser()).then(()=> setIsLoaded(true));
+  },[dispatch])
+
   return (
     <Switch>
       <Route path="/login">
