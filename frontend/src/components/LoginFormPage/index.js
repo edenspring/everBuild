78b0e663c9964e7f69bdf88eae54e75074eecb5d
Bill Adams <vikingbill@gmail.com>
2021-05-20 17:37:21 -0400

    add LoginFormPage component

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+
+import './LoginForm.css';
+
+export default function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
+  };
+
+  return (
+    <div className="login-form">
+      <h2>login placeholder</h2>
+      <form onSubmit={onSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <label>
+          Please enter your username or email address
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+          />
+        </label>
+        <label>
+          Please enter yourpassword
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+}
