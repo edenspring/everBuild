505ea00e9058e25feb27885c6bc19375e66c2bd9
Bill Adams <vikingbill@gmail.com>
2021-05-20 17:13:29 -0400

    session store in frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,24 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
+import App from './App';
 import configureStore from './store';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
