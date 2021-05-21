044f3693215ded067ac71886489a3afa22a150b8
Bill Adams <vikingbill@gmail.com>
2021-05-21 14:27:21 -0400

    add finished front and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import * as sessionActions from "./store/session"
+import * as sessionActions from "./store/session";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
+import Navigation from "./components/Navigation";
 
 function App() {
-
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(()=>{
-    dispatch(sessionActions.restoreUser()).then(()=> setIsLoaded(true));
-  },[dispatch])
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
 
   return (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-    </Switch>
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 
