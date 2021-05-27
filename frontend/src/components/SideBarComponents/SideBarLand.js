0d110d2ab92c773ed08c4ccfa60d6059d694ed15
Bill Adams <vikingbill@gmail.com>
2021-05-27 00:31:58 -0400

    progress with styling

--- frontend/src/components/SideBarComponents/SideBarLand.js
+++ frontend/src/components/SideBarComponents/SideBarLand.js
@@ -7,9 +7,11 @@ import SideBarPlace from "./SideBarPlace";
 function SideBarLand() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
-  const currentLand = useSelector((state)=>state.land.land);
-  const currentPlace = useSelector((state)=>state.place);
+  const currentLand = useSelector((state) => state.land.land);
+  const currentPlace = useSelector((state) => state.place);
   const [userLands, setUserLands] = useState([]);
+  const [expand, setExpand] = useState(false);
+
   // console.log(sessionUser);
   useEffect(() => {
     dispatch(landActions.getUserLands(sessionUser.id)).then((data) =>
@@ -20,21 +22,40 @@ function SideBarLand() {
   // const userLands = useSelector((state) => state.land.userLands)
 
   console.log(userLands);
+  function iconHover(e) {
+    const div = e.target.closest('.individual__land');
+    const icon = div.querySelector('i');
+    icon.classList.remove("fa-angle-right");
+    icon.classList.add("fa-angle-down");
+  }
+
+  function iconDepart(e) {
+    const div = e.target.closest('.individual__land');
+    const icon = div.querySelector('i');
+    icon.classList.remove("fa-angle-down");
+    icon.classList.add("fa-angle-right");
+  }
 
   return (
     <div className="userlands__div">
-      {userLands.map((e, i) =>
-        <div key={i} className='individual__land sidebar__content'>
-          <NavLink className='land__link' exact to={`/lands/${e.id}`} >
-            <div className='link__container__div'>{e.name}</div>
+      {userLands.map((e, i) => (
+        <div key={i} className="individual__land sidebar__content" onMouseOver={iconHover} onMouseOut={iconDepart}>
+          <NavLink className="land__link" exact to={`/lands/${e.id}`}>
+            <div className="link__container__div">
+              <i id='icon'class="fas fa-angle-right" ></i>
+              <span>{` `}</span>
+              {("    ", e.name)}
+            </div>
           </NavLink>
           {e.Places && (
             <ul>
-              {e.Places.map((place, idx)=> <SideBarPlace place={place} key={idx} />)}
+              {e.Places.map((place, idx) => (
+                <SideBarPlace place={place} key={idx} />
+              ))}
             </ul>
           )}
         </div>
-      )}
+      ))}
     </div>
   );
 }
